b4eca248a942c9456843504b6a0206974b6470b4
vayu02 <ashish02.shejwal@gmail.com>
2021-05-27 03:42:04 +0530

    created components board square & changed title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Board from "./components/Board"
 
 
 const App = () =>{
-<>
-<h1>Welcome to React Parcel Micro App!</h1>
-<p>Hard to get more minimal than this React app.</p>
-</>
+    return(
+        <div>
+            <h1>Tic Tac Toe</h1>
+            <Board />
+        </div>
+    );
 }
 
 
